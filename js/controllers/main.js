6b9890a0682e3f2e63b32d7e435aeedc7f87b203
witch <songyanshan.2008@163.com>
2017-03-02 01:39:00 +0800

    20170302_01:38_completed

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -7,8 +7,8 @@
 	*
 	* description
 	*/
-	//创建一个独立的模块，将其注入到app模块中
-	//或者取得app模块var app = module('app')再写控制器(但要先引入app.js)
+	//创建一个独立的模块，将其注入到app模块中(html中先引入main.js再引入app.js)
+	//或者先取得app模块var app = module('app')再写控制器(html中先引入app.js)
 	var controllers = angular.module('app.controllers.main',['app.services.main']);
 	controllers.controller('mainController',[
 		'$scope',
@@ -16,35 +16,47 @@
 		'$route',
 		'mainService',
 		function($scope,$routeParams,$route,mainService){
-		//初始化模型
+		//---------------初始化模型-------------------
 		//文本框需要一个模型，为了拿到文本输入的值
 		$scope.text = '';
 
 		//任务列表也需要一个模型
 		//每一个任务的结构{ id:1, text: '学习', completed: true }
 		$scope.todos = mainService.get()
+		//---------------------------------------------
 
 		//处理增加
 		$scope.add = function() {
 			//参数校验 界面逻辑
-			if(!scope.text) {
+			if(!$scope.text) {
 				return;
 			}
 			mainService.add($scope.text);
-			$scope.text = ''  //清空文本框
+			$scope.text = '';  //清空文本框，界面逻辑
 		}
 
 		//处理删除
 		$scope.remove = function(id){
-			//此处是界面逻辑
+			//此处是界面逻辑但并不存在界面逻辑
+			//所以可以直接这样写
+			//$scope.remove = mainService.remove(id)
+			//但不建议这样写
 			mainService.remove(id)
 		}
 
-		//清空
-		$scope.clear = mainService.existCompleted;
+		//清空已完成的任务
+		//注意变量地址
+		$scope.clearCompleted = function() {
+			var newTodos = mainService.clearCompleted();
+			$scope.todos = newTodos;
+		}
 
-		//当前编辑元素，没有用到数据模型
+      	//是否有已经完成的
+      	$scope.existCompleted = mainService.existCompleted;
+
+		//当前编辑元素，不存在数据交互
 		$scope.currentEditingId = -1;
+		//-1代表一个肯定不存在的元素，默认没有任何编辑
 		$scope.editing = function(id) {
 			//界面逻辑
 			$scope.currentEditingId = id;
@@ -55,7 +67,11 @@
 
 		$scope.toggleAll = mainService.toggleAll;
 
-		//筛选(路由)
+		$scope.toggle = function() {
+			mainService.save()
+		}
+
+		//筛选(路由)，无数据交互
 		$scope.selector = {};  // {} {completed:true} {completed:false}
 		console.log($routeParams);
 		var status = $routeParams.status;
--- js/services/main.js
+++ js/services/main.js
@@ -1,90 +1,101 @@
-(function(angular){
-	//注册一个新模块，作为服务模块
-	//将该模块注入controllers下的模块
-	 angular.module('app.service.main',[])
-	 .service('mainService',[function() {
-	 	//业务逻辑都必须出现在服务中(专门定义业务逻辑)
-	 	//添加todo
-	 	var todos = [
-		{ id : 0.01, text : '学习', completed : false},
-		{ id : 0.02, text : '睡觉', completed : false},
-		{ id : 0.03, text : '打豆豆', completed : true}
-		];
+(function(angular) {
+  //注册一个新模块
+  angular.module('app.services.main',[])
+    .service('mainService',['$window',function($window) {
+        // var todos = [
+        // { id : 0.01, text : '学习', completed : false},
+        // { id : 0.02, text : '睡觉', completed : false},
+        // { id : 0.03, text : '打豆豆', completed : true}
+        // ]; 
+        var storage = $window.localStorage;
+        console.log(storage['mytodoList']);
+        var todos = storage['mytodoList'] ? JSON.parse(storage['mytodoList']) : [];
+        console.log(todos);
 
-		this.add = function(text) {
-			todos.push({
-				//自动增长
-				id : getId(),
-				// 由于$scope默认是双向绑定的，add的同时肯定可以拿到界面上
-				// 的输入值
-				text : text,
-				completed : false
-			});
-			//清除模型数据--->清除文本框
-			text = '';
-		};
+        //当todos发生改变时，执行save()
+        this.save = function() {
+            storage['mytodoList'] = JSON.stringify(todos);
+        }
 
-		//删除
-		this.remove = function(id) {
-			//删除谁
-			for (var i = 0,len = todos.length; i < len; i++) {
-				if(todos[i].id === id) {
-					todos.splice(i,1);
-					break;
-				}
-			}
-		};
+        // 获取唯一ID
+        function getId() {
+            var id = Math.random(); // 1 2
+            for (var i = 0; i < todos.length; i++) {
+              if (todos[i].id === id) {
+                id = getId();
+                break;
+              }
+            }
+            return id;
+        }
 
-		//清空
-		this.clear = function() {
-			var result = [];
-			for (var i = 0; i < todos.length; i++) {
-				if(!todos[i].completed) {
-					result.push(todos[i]);
-				}
-			}
-			todos = result
-		};
+        //控制私有字段的访问权限
+        //todos放外面--->更可控
+        this.get = function(){
+            return todos;
+        }
 
-		//是否已经有完成的
-		//若ng-show绑定函数，该函数必须返回布尔值
-		this.existCompleted = function() {
-			for (var i = 0, len = todos.length; i < len; i++) {
-				if(todos[i].completed){
-					return true;
-				}
-			}
-			return false;
-		};
+        //业务逻辑都必须出现在服务中(专门定义业务逻辑)
+        this.add = function(text) {
+            todos.push({
+              // 自动增长？
+              id: getId(),
+              // 由于$scope.text是双向绑定的，add同时肯定可以同他拿到界面上的输入
+              text: text,
+              completed: false
+            });
+            this.save(todos);
+        };
 
-		//更新
-		// this.update = function(id , target) {
+        //处理删除
+        this.remove = function(id) {
+            for (var i = 0; i < todos.length; i++) {
+              if (todos[i].id === id) {
+                todos.splice(i, 1);
+                break;
+              }
+            }
+            this.save(todos);
+        };
+        
+        //清空已完成的
+        this.clearCompleted = function() {
+            var result = [];
+            for (var i = 0; i < todos.length; i++) {
+              if (!todos[i].completed) {
+                result.push(todos[i]);
+              }
+            }
+            todos = result;
+            //此时todos指向了一个新的地址
+            //$scope.todos 与 todos 不再共享一个地址
+            //即$scope.todos并没发生改变
+            this.save(todos);
+            return todos;
+        }
 
-		// }
+        // 是否有已经完成的
+        this.existCompleted = function() {
+        for (var i = 0; i < todos.length; i++) {
+            if (todos[i].completed) {
+                return true;
+            }
+          }
+          return false;
+        };
 
-		var now = true;
-		this.toggleAll = function(){
-			for (var i = 0; i < todos.length; i++) {
-				todos[i].completed = now;
-			}
-			now = !now;
-		};
+        
+        //toggleAll开关
+        var now = true;
+        this.toggleAll = function() {
+        for (var i = 0; i < todos.length; i++) {
+            todos[i].completed = now;    //数据交互，业务逻辑
+          }   
+            now = !now;
+            this.save(todos)
+        };
+    }]);
+})(angular);
+	 	
 
-		//控制私有字段的访问权限
-		this.get = function() {
-			return todos;
-		}
-		
-		//完美处理id，但效率低
-		function getId(){
-			var id = Math.random();
-			for (var i = 0, len = todos.length; i < len; i++) {
-				if(todos[i].id === id){
-					id = getId();
-					break;
-				}
-			}
-			return id;
-		}
-	 }])
-})(angular)
+	 	
